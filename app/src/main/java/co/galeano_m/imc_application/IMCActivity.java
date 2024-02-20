244d6eb9e9e22824008e78109c6b4b0d94c6484b
Miguel Ángel Galeano <113398998+ElMiGuElG@users.noreply.github.com>
2024-02-19 19:18:11 -0500

    The XML documents were added

---
+++ app/src/main/java/co/galeano_m/imc_application/IMCActivity.java
@@ -0,0 +1,14 @@
+package co.galeano_m.imc_application;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class IMCActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_imcactivity);
+    }
+}
